27a884d05ebf086891d875d0602de63eaa6e06f5
unknown <vex.kim@kps.systems>
2022-03-02 17:09:35 +0900

    feature[basic_setting] : 기본 세팅
    * gradle
    * java
    * spring-data-jpa
    * h2database
    * lombok
    * spring-security

---
+++ src/main/java/intulda/poly/assignment/PolyAssignmentApplication.java
@@ -0,0 +1,13 @@
+package intulda.poly.assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolyAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PolyAssignmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/intulda/poly/assignment/PolyAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package intulda.poly.assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolyAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
