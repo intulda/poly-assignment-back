e2ee23a2449416ce78bb45eecc22af18572e2070
unknown <vex.kim@kps.systems>
2022-03-03 10:40:57 +0900

    feature[spring-security] : JWT 세팅을 위한 기본적인 security 세팅

---
+++ src/main/java/intulda/poly/assignment/global/configuration/security/configuration/WebSecurityConfig.java
@@ -0,0 +1,35 @@
+package intulda.poly.assignment.global.configuration.security.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf()
+                .disable()
+                .formLogin()
+                .disable()
+                .authorizeRequests()
+                .antMatchers("/authenticate").permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
